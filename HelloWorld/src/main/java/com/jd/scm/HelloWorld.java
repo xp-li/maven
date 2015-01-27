60d9f6b297f36542dabd0996d1fbce3276df465c
xp-li <lixuepan@gmail.com>
2015-01-27 15:16:05 +0800

    New maven learn project.

---
+++ HelloWorld/src/main/java/com/jd/scm/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.jd.scm;
+
+public class HelloWorld 
+{
+	public String sayHello()
+	{
+		return "Hello Maven";
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.print(new HelloWorld().sayHello());
+	}
+
+}
